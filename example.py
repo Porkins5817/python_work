d42bf71591e3bfc8602d74cae5403d747248247c
Porkins <poomster@gmail.com>
2021-02-04 15:48:48 -0800

    programming example

---
+++ example.py
@@ -0,0 +1,16 @@
+# This code will display three messages to the user
+#first message will be Hello World
+#Second message Uni-Rex
+#define 3 variables, Name, Lname, and fullname
+#combine all 3 variables
+#display final message using all 3 variables
+
+print("Hello World") #display print Hello World
+
+print("Uni-Rex") # second message to be displayed
+
+Name = "Jerry" # first name
+Lname = "Martinez" # users last name
+fullname = f"{Name} {Lname}" # combine first with last name
+print(fullname) #display full name
+
